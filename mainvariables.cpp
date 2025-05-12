b247cefa3c71cc9602d7d63387126e2756d553cd
252pollodz <157359612+252pollodz@users.noreply.github.com>
2025-05-12 12:50:13 +0200

    mainvariables.cpp

--- mainvariables.cpp
+++ mainvariables.cpp
@@ -7,30 +7,31 @@ void usage(char *program_name) {
     exit(1);
 }
 int main(int argc, char *argv[]) {
-    int firstnumber, secondnumber,result;
+    int first_number, second_number,result;
     char *operation;
     if (argc < 3)
         usage(argv[0]);
-    firstnumber = atoi(argv[1]);
-    secondnumber = atoi(argv[3]);
+    first_number = atoi(argv[1]);
+    second_number = atoi(argv[3]);
     operation = argv[2];
     if (strcmp(operation, "add") == 0) {
-        result = firstnumber + secondnumber;
-        printf("%d + %d = %d\n",firstnumber,secondnumber, result);
+        result = first_number + second_number;
+        printf("%d + %d = %d\n",first_number,second_number, result);
     }else if (strcmp(operation, "sub") == 0) {
-        result = firstnumber - secondnumber;
-        printf("%d - %d = %d\n",firstnumber,secondnumber, result);
+        result = first_number - second_number;
+        printf("%d - %d = %d\n",first_number,second_number, result);
 
     }else if (strcmp(operation, "multiply") == 0) {
-        result = firstnumber * secondnumber;
-        printf("%d * %d = %d\n",firstnumber,secondnumber, result);
+        result = first_number * second_number;
+        printf("%d * %d = %d\n",first_number,second_number, result);
     }else if (strcmp(operation, "divide") == 0) {
-        if(secondnumber == 0) {
+        if(second_number == 0) {
             printf("Nie mozna dzielic przez 0!!!");
             exit(1);
         }else {
-            result = firstnumber / secondnumber;
-            printf("%d / %d = %d\n",firstnumber,secondnumber, result);
+            float result_of_dividing;
+            result_of_dividing = (float)first_number / (float)second_number;
+            printf("%d / %d = %f\n",first_number,second_number, result_of_dividing);
         }
     }
     return 0;
